f96f118754839fb4bffb0ac467c6a0f3545e5241
Vaibhav Wankhede <59387229+7028371795@users.noreply.github.com>
2023-10-04 09:06:30 +0000

    String Done

---
+++ 05_String/string.js
@@ -0,0 +1,25 @@
+let mynName="vaibhav";
+let repoCount=50;
+// console.log(myName + repoCount);
+
+console.log(`my name is ${mynName} and my repo Count is ${repoCount}`);
+
+const gameName=new String("Wankhede");
+
+console.log(gameName);
+console.log(gameName[0]);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('k'));
+
+let newString=gameName.substring(0,4);
+console.log(newString);
+
+let anotherString=gameName.slice(-8,4);
+console.log(anotherString);
+
+const newStringOne="    Vaibhav     ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
