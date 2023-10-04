7f9e6a058c6743bcbfaa9063aa1ee5dceed3d60c
Vaibhav Wankhede <59387229+7028371795@users.noreply.github.com>
2023-10-04 09:35:28 +0000

    Number ,Math and its Methods

---
+++ 06_Num_Math/math.js
@@ -0,0 +1,15 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.7));
+console.log(Math.round(4.2));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+
+let min=10;
+let max=30;
+console.log(Math.floor(Math.random()*(max-min+1))+1);
+
+
---
+++ 06_Num_Math/num.js
@@ -0,0 +1,15 @@
+const score=400;
+console.log(score);
+const balance=new Number(1000);
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+let newNumber=123.209384;
+console.log(newNumber.toPrecision(4));
+
+let otherNum=10000000;
+console.log(otherNum.toLocaleString('en-IN'));
+
