03450dd7a8becef4df1cc021c38d910c13d206a0
Vaibhav Wankhede <59387229+7028371795@users.noreply.github.com>
2023-10-04 04:00:10 +0000

    Variable and datatypes Done

---
+++ 01_Variable/var.js
@@ -0,0 +1,21 @@
+const customerId=123;
+let email="vaibhav@gmail.com";
+var Name="vaibahv";
+
+//customerId=321; // not allowed to reasign value to constant Varibale
+ console.log(customerId);  
+
+Name="vaibhav wankhede";
+console.log(Name);
+/*
+Prefer not use Var 
+because of issue in block scope and functional scope
+*/
+
+console.log(email);
+
+console.table([customerId,email,Name]);
+
+/*
+ use let and const 
+*/
---
+++ 02_DataType/datatype.js
@@ -0,0 +1,24 @@
+// alert(3+3); //we are using node js ,not browser
+
+console.log(
+    3+3 
+)//code relability high
+
+console.log("Vaibhav");
+console.log(25);
+console.log(false);
+
+let Name="vaibhav";
+let age=24;
+let isMarried=false;
+
+//number 2 power of 53
+//bigint
+//string
+//boolean =>true or false
+//null => stand alone value
+//undefine =>when u can not assing the value to variable
+//Symbol
+
+console.log(typeof undefine);// undefined
+console.log(typeof null);// object
