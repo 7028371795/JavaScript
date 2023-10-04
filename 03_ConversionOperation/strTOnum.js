5446ed1dcffa1db63e2bd6edc1192ce196908ad3
Vaibhav Wankhede <59387229+7028371795@users.noreply.github.com>
2023-10-04 05:18:08 +0000

    complecated conversiona and perfix and postfix Done

---
+++ 03_ConversionOperation/strTOnum.js
@@ -0,0 +1,35 @@
+let value=33;
+let negValue=-33;
+console.log(negValue);
+
+// /**Operators */
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="Vaibahv";
+let str2="wankhede";
+console.log(str1+str2);
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+3+"2");
+
+console.log(+true);
+console.log(+"");
+
+let a=100;
+console.log(a++);
+console.log(a);
+
+a=100;
+console.log(++a);
+
+
+
+
